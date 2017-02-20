4ff360547e734b084724f1a43f82b4b153c11fae
Hannah Mungamuri <hannahmoses612@gmail.com>
2017-02-20 14:28:47 -0600

    valid Bday error output uses string substitution !

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import webapp2
 header="<h2 style='color:white;background-color:rgb(145,0,0);text-align:center'>WELCOME </h2>"
 form="""<form method="post"  style='color:rgb(145,0,0);background-color:pink'>
     <div style='color:green'><h2>%(error)s</h2></div>
-    <body style='color:rgb(145,0,0);background-color:pink' ><p>Please enter your Bday</p>
-        <label>Month <input type="text" name="month" ></label> <br><br>
-    	<label>Day <input type="text" name="day"></label><br><br>
-        <label>Year <input type="text" name="year"></label><br><br>
+    <body style='color:rgb(145,0,0);background-color:pink' ><p>Please enter your birthday</p>
+        <label>Month <input type="text" name="month" value=%(month)s ></label> <br><br>
+    	<label>Day <input type="text" name="day" value=%(day)s></label><br><br>
+        <label>Year <input type="text" name="year" value=%(year)s></label><br><br>
         <br><br>
         <input type="submit" value="Submit Bday"style='color:white;background-color:rgb(145,0,0)'>
     </body>
@@ -35,18 +35,23 @@ def valid_year(year):
         if (1900<year<2020):
             return year
 class MainPage(webapp2.RequestHandler):
-    def write_form(self,error=""):
-        self.response.out.write(form % {"error": error})
+    def write_form(self,error="",month="",day="",year=""):
+        self.response.out.write(form % {"error": error,"month":month,"day":day,"year":year})
     def get(self):
         self.response.out.write(header)
         self.write_form()
     def post(self):
-        user_month=valid_month(self.request.get("month"))
-        user_day = valid_day(self.request.get("day"))
-        user_year = valid_year(self.request.get("year"))
-        if not(user_month and user_day and user_year):
-            outmessage="That is not a valid Bday !"
-            self.write_form(outmessage)
+        user_month=(self.request.get("month"))
+        user_day = (self.request.get("day"))
+        user_year =(self.request.get("year"))
+
+        month = valid_month(user_month)
+        day = valid_day(user_day)
+        year = valid_year(user_year)
+
+        if not(month and day and year):
+            outmessage="That is not a valid birthday !"
+            self.write_form(outmessage,user_month,user_day,user_year)
         else:
             outhead =" <h2 style='background-color:rgb(200,134,125)'> Thanks for entering good data !</h2>"
             self.response.write(outhead + outform)
